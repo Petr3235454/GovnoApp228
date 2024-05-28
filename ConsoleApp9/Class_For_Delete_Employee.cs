40bb35a1fe8153ec45509dcf00513da3f631d70f
Petr3235454 <petrponamarev66@gmail.com>
2024-05-28 22:13:53 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Class_For_Delete_Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Class_For_Delete_Employee
+    {
+        public void DeleteEmployee(List<Class_For_Add_Employee.Employee> EMPLOYEE_MAIN_LIST)
+        {
+            Console.Clear();
+
+            if (EMPLOYEE_MAIN_LIST.Count == 0)
+            {
+                Console.WriteLine("\nСписок сотрудников пустОЙЙАААААА.\n");
+                return;
+            }
+
+            Console.WriteLine("Выберите сотрудника для удаления:");
+
+            for (int i = 0; i < EMPLOYEE_MAIN_LIST.Count; i++)
+            {
+                Console.WriteLine($"[{i + 1}] {EMPLOYEE_MAIN_LIST[i].Name} {EMPLOYEE_MAIN_LIST[i].Surname} {EMPLOYEE_MAIN_LIST[i].MonthSalary} {EMPLOYEE_MAIN_LIST[i].PhoneNumber}");
+            }
+
+            int index = int.Parse(Console.ReadLine());
+            if (index > 0 && index <= EMPLOYEE_MAIN_LIST.Count)
+            {
+                EMPLOYEE_MAIN_LIST.RemoveAt(index - 1);
+                Console.WriteLine("Сотрудник удален.");
+            }
+            else
+            {
+                Console.WriteLine("Неверный выбор.");
+            }
+
+            Console.WriteLine("\nНажмите любую клавишу для продолжения...");
+            Console.ReadKey(true);
+            Console.Clear();
+        }
+    }
+}
---
+++ ConsoleApp9/Class_for_the employee_display_method.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Class_For_The_Employee_Display_Method
+    {
+        public void ShowEmployees(List<Class_For_Add_Employee.Employee> EMPLOYEE_MAIN_LIST)
+        {
+            Console.Clear();
+            Console.WriteLine("\nСписок сотрудников:\n");
+
+            foreach (var employee in EMPLOYEE_MAIN_LIST)
+            {
+                Console.WriteLine($"Имя: {employee.Name}, Фамилия: {employee.Surname}, Зарплата: {employee.MonthSalary}, Телефон: {employee.PhoneNumber}");
+            }
+
+           if (EMPLOYEE_MAIN_LIST.Count == 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Список пуст!");
+            }
+            
+
+            Console.WriteLine("\n [Назад] Нажмите любую клавишу для продолжения...");
+            Console.ReadKey(true);
+            Console.Clear();
+        }
+    }
+}
